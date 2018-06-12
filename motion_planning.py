466f64999caf5fd6bd20374ca55ccb9e6b4f1a2d
Ramsri <ramsrigouthamg@gmail.com>
2018-06-13 00:10:20 +0530

    Adding path pruning

--- motion_planning.py
+++ motion_planning.py
@@ -12,6 +12,16 @@ from udacidrone.messaging import MsgID
 from udacidrone.frame_utils import global_to_local
 
 
+def point(p):
+    return np.array([p[0], p[1], 1.]).reshape(1, -1)
+
+
+def collinearity_check(p1, p2, p3, epsilon=1e-6):
+    m = np.concatenate((p1, p2, p3), 0)
+    det = np.linalg.det(m)
+    return abs(det) < epsilon
+
+
 class States(Enum):
     MANUAL = auto()
     ARMING = auto()
@@ -111,6 +121,29 @@ class MotionPlanning(Drone):
         data = msgpack.dumps(self.waypoints)
         self.connection._master.write(data)
 
+    # We're using collinearity here, but you could use Bresenham as well!
+    def prune_path(path):
+        pruned_path = [p for p in path]
+        i = 0
+        while i < len(pruned_path) - 2:
+            p1 = point(pruned_path[i])
+            p2 = point(pruned_path[i + 1])
+            p3 = point(pruned_path[i + 2])
+
+            # If the 3 points are in a line remove
+            # the 2nd point.
+            # The 3rd point now becomes and 2nd point
+            # and the check is redone with a new third point
+            # on the next iteration.
+            if collinearity_check(p1, p2, p3):
+                # Something subtle here but we can mutate
+                # `pruned_path` freely because the length
+                # of the list is check on every iteration.
+                pruned_path.remove(pruned_path[i + 1])
+            else:
+                i += 1
+        return pruned_path
+
     def plan_path(self):
         self.flight_state = States.PLANNING
         print("Searching for a path ...")
@@ -160,8 +193,11 @@ class MotionPlanning(Drone):
         # TODO: prune path to minimize number of waypoints
         # TODO (if you're feeling ambitious): Try a different approach altogether!
         print (path)
+        pruned_path = self.prune_path(path)
+        print(len(pruned_path))
+
         # Convert path to waypoints
-        waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
+        waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in pruned_path]
         # Set self.waypoints
         self.waypoints = waypoints
         # TODO: send waypoints to sim (this is just for visualization of waypoints)
