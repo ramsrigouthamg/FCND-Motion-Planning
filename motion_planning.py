b4772ada886a3ebdff593c02b5888a370a1c0208
Ramsri <ramsrigouthamg@gmail.com>
2018-06-12 20:23:04 +0530

    Finishing intial to-do's

--- motion_planning.py
+++ motion_planning.py
@@ -2,7 +2,7 @@ import argparse
 import time
 import msgpack
 from enum import Enum, auto
-
+import csv
 import numpy as np
 
 from planning_utils import a_star, heuristic, create_grid
@@ -120,13 +120,19 @@ class MotionPlanning(Drone):
         self.target_position[2] = TARGET_ALTITUDE
 
         # TODO: read lat0, lon0 from colliders into floating point values
-        
+        with open('colliders.csv','r') as f:
+            reader = csv.reader(f,delimiter=',')
+            row1 = next(reader)
+        lat0 = float(row1[0].split()[1])
+        lon0 = float(row1[1].split()[1])
+        print ("lat0: ",lat0," lon0: ",lon0)
         # TODO: set home position to (lon0, lat0, 0)
-
+        self.set_home_position(lon0, lat0, 0)
         # TODO: retrieve current global position
- 
+        current_global_pos = self.global_position
         # TODO: convert to current local position using global_to_local()
-        
+        current_local_position = global_to_local(current_global_pos,self.global_home)
+
         print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
                                                                          self.local_position))
         # Read in obstacle map
@@ -150,7 +156,7 @@ class MotionPlanning(Drone):
         path, _ = a_star(grid, heuristic, grid_start, grid_goal)
         # TODO: prune path to minimize number of waypoints
         # TODO (if you're feeling ambitious): Try a different approach altogether!
-
+        print (path)
         # Convert path to waypoints
         waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
         # Set self.waypoints
