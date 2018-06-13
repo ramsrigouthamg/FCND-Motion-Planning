0ba432f92c0fdbc7e7c2a9c5f6e410327a4b8630
Ramsri <ramsrigouthamg@gmail.com>
2018-06-13 11:13:20 +0530

    Final working version in all four directions

--- motion_planning.py
+++ motion_planning.py
@@ -5,44 +5,11 @@ from enum import Enum, auto
 import csv
 import numpy as np
 
-from planning_utils import a_star, heuristic, create_grid
+from planning_utils import a_star, heuristic, create_grid, prune_path
 from udacidrone import Drone
 from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
 from udacidrone.frame_utils import global_to_local
-
-
-def point(p):
-    return np.array([p[0], p[1], 1.]).reshape(1, -1)
-
-
-def collinearity_check(p1, p2, p3, epsilon=1e-6):
-    m = np.concatenate((p1, p2, p3), 0)
-    det = np.linalg.det(m)
-    return abs(det) < epsilon
-
-# We're using collinearity here, but you could use Bresenham as well!
-def prune_path(path):
-    pruned_path = [p for p in path]
-    i = 0
-    while i < len(pruned_path) - 2:
-        p1 = point(pruned_path[i])
-        p2 = point(pruned_path[i + 1])
-        p3 = point(pruned_path[i + 2])
-
-        # If the 3 points are in a line remove
-        # the 2nd point.
-        # The 3rd point now becomes and 2nd point
-        # and the check is redone with a new third point
-        # on the next iteration.
-        if collinearity_check(p1, p2, p3):
-            # Something subtle here but we can mutate
-            # `pruned_path` freely because the length
-            # of the list is check on every iteration.
-            pruned_path.remove(pruned_path[i + 1])
-        else:
-            i += 1
-    return pruned_path
 
 
 class States(Enum):
@@ -183,7 +150,10 @@ class MotionPlanning(Drone):
         # TODO: adapt to set goal as latitude / longitude position and convert
 
         # working point north east (-122.396046, 37.794463,0)
-        grid_goal_lon_lat = (-122.396046, 37.794463,0)
+        # working point north west (-122.399307, 37.794398,0)
+        # working point south west (-122.397907, 37.791083,0)
+        # working point south east (-122.394870, 37.791238,0)
+        grid_goal_lon_lat = (-122.394870, 37.791238,0)
         goal_local_position = global_to_local(grid_goal_lon_lat, self.global_home)
         print ("current_local_position ",current_local_position,"  goal_local_position ",goal_local_position)
         grid_goal = (int(-north_offset + goal_local_position[0]), int(-east_offset + goal_local_position[1]))
