c47875ca0a2dc846ce6ccf68c0baa7ac8ee068a3
Ramsri <ramsrigouthamg@gmail.com>
2018-06-13 09:40:08 +0530

    Moving prunepath to its location

--- motion_planning.py
+++ motion_planning.py
@@ -21,6 +21,29 @@ def collinearity_check(p1, p2, p3, epsilon=1e-6):
     det = np.linalg.det(m)
     return abs(det) < epsilon
 
+# We're using collinearity here, but you could use Bresenham as well!
+def prune_path(path):
+    pruned_path = [p for p in path]
+    i = 0
+    while i < len(pruned_path) - 2:
+        p1 = point(pruned_path[i])
+        p2 = point(pruned_path[i + 1])
+        p3 = point(pruned_path[i + 2])
+
+        # If the 3 points are in a line remove
+        # the 2nd point.
+        # The 3rd point now becomes and 2nd point
+        # and the check is redone with a new third point
+        # on the next iteration.
+        if collinearity_check(p1, p2, p3):
+            # Something subtle here but we can mutate
+            # `pruned_path` freely because the length
+            # of the list is check on every iteration.
+            pruned_path.remove(pruned_path[i + 1])
+        else:
+            i += 1
+    return pruned_path
+
 
 class States(Enum):
     MANUAL = auto()
@@ -121,29 +144,6 @@ class MotionPlanning(Drone):
         data = msgpack.dumps(self.waypoints)
         self.connection._master.write(data)
 
-    # We're using collinearity here, but you could use Bresenham as well!
-    def prune_path(path):
-        pruned_path = [p for p in path]
-        i = 0
-        while i < len(pruned_path) - 2:
-            p1 = point(pruned_path[i])
-            p2 = point(pruned_path[i + 1])
-            p3 = point(pruned_path[i + 2])
-
-            # If the 3 points are in a line remove
-            # the 2nd point.
-            # The 3rd point now becomes and 2nd point
-            # and the check is redone with a new third point
-            # on the next iteration.
-            if collinearity_check(p1, p2, p3):
-                # Something subtle here but we can mutate
-                # `pruned_path` freely because the length
-                # of the list is check on every iteration.
-                pruned_path.remove(pruned_path[i + 1])
-            else:
-                i += 1
-        return pruned_path
-
     def plan_path(self):
         self.flight_state = States.PLANNING
         print("Searching for a path ...")
@@ -196,7 +196,7 @@ class MotionPlanning(Drone):
         # TODO: prune path to minimize number of waypoints
         # TODO (if you're feeling ambitious): Try a different approach altogether!
         print (path)
-        pruned_path = self.prune_path(path)
+        pruned_path = prune_path(path)
         print(len(pruned_path))
 
         # Convert path to waypoints
